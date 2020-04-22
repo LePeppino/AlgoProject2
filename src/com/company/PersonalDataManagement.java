fe1213340ea843edb52e362795d7341114a26a8d
EricWalter5174 <eric.walter@tesos.de>
2020-04-22 15:04:25 +0200

    some tweaking, PersonalDataManagement not implemented yet

--- src/com/company/PersonalDataManagement.java
+++ src/com/company/PersonalDataManagement.java
@@ -1,10 +1,20 @@
 package com.company;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class PersonalDataManagement {
 
     public static void main(String[] args) {
 
         var rbt = new PersonalData();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Personaldata Management System 1.0 \n Specify absolute path to the data file:");
+
+        String path = input.nextLine();
+        File file = new File(path);
+
 
     }
 
