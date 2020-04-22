89482253158fb3b7b840cf5cc53c4977f7d698ef
EricWalter5174 <eric.walter@tesos.de>
2020-04-22 16:10:34 +0200

    some tweaking, PersonalDataManagement not implemented yet

--- src/com/company/PersonalDataManagement.java
+++ src/com/company/PersonalDataManagement.java
@@ -8,16 +8,18 @@ public class PersonalDataManagement {
 
     public static void main(String[] args) {
         var rbt = new PersonalData();
+        int counter = 0;
         String surname;
         String name;
-        Integer age;
+        int age;
         String gender;
-        Scanner input = new Scanner(System.in);
+        var input = new Scanner(System.in);
         Scanner reader = null;
 
         System.out.println("Personaldata Management System 1.0 \n Specify absolute path to the data file:");
 
         String path = input.nextLine();
+        input.close();
 
         try {
             reader = new Scanner(new File(path)).useDelimiter("\\s*,\\s*");
@@ -29,12 +31,12 @@ public class PersonalDataManagement {
         assert reader != null;
 
         while(reader.hasNext()){
-            int i = 0;
             surname = reader.next();
             name= reader.next();
             age = reader.nextInt();
             gender = reader.next();
-            rbt.insert(i, new Person(surname, name, age, gender));
+            rbt.insert(counter, new Person(surname, name, age, gender));
+            counter++;
         }
         reader.close();
 
